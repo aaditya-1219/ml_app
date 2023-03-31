bdcd48dc788e25c3a48dedf3196c48535c3a4d40
aaditya-1219 <aadityapatil1910@gmail.com>
2023-03-31 14:04:01 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+      ),
+      home: const Home(),
+    );
+  }
+}
